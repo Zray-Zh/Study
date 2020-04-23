9ae7762a70aa4e632d5cf5ed8fbf13e8a37f3085
Zray-Zh <16657130@qq.com>
2020-04-23 14:14:16 +0800

    Study

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -6,6 +6,6 @@ import java.sql.SQLOutput;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("Hello Git II!");
+        System.out.println("Hello Git!");
     }
 }
